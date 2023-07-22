b9d192ccfd37234e55e13f07fb6ea99a11119710
Annette Haas <ahaas@sd1.org>
2023-07-22 16:31:37 -0400

    project update 3

--- clean.py
+++ clean.py
@@ -10,25 +10,55 @@ import numpy as np
 # Olympic_Data.csv    = C:\Users\ahaas.SDDOMAIN\Desktop\Olympic_Data.csv\data
 # data/Population     = C:\Users\ahaas.SDDOMAIN\Desktop\Population.csv\data
 
+import pandas as pd
+import numpy as np
+import select as sql   
+
 # Read in Data files
 P1_df = pd.read_csv('data/Olympic_Data.csv')
 P2_df = pd.read_csv('data/Population.csv')
 
 # drop unneeded columns
-P2_df.drop(P2_df.columns[0,2,3], axis =1, inplace=True)
+P2_df=P2_df.drop(['Rank', 'Country/Territory', 'Captial', 'Continent'], axis =1)
+P1_df=P1_df.drop(['ID', 'Games','Height', 'Weight', 'Team'], axis =1)
+
+# drop olympians that didnt medal
+P1_df.dropna(subset = ['Medal'], inplace=True)
+P1_df.head()
 
 # rename columns
-fixed_columns = {
-    'Country Code':'NOC',
-}
+P2_df.rename(columns = {'CCA3':'NOC'}, inplace = True)
 
-P2_df.rename(columns=fixed_columns,inplace=True)
-P2_df.columns
+#identify NOC that is not the same
+P1_df[P1_df['df1']['NOC'].isin(P2_df['df2']['NOC'])]
+
 
 # converting to number
-P2_df_int = {
-     'Number': ':,',
-}
+#year=1960
+#while year!=2022:
+    #year=str(year)
+    #P2_df[year]=P2_df[year].fillna(0)
+    #for row in P2_df[year]:
+        #row=int(row)
+        #new_row=("{:,}".format(row))
+        #P2_df[year]=P2_df[year].replace([row],new_row)
+    #year=int(year)+1
+#P2_df.head()
 
+#P1_df.head()
 
-## merge and clean data
+#num_of_rows = P1_df.shape
+#print(num_of_rows)
+
+# merge data sets
+
+output4 = pd.merge(P1_df, P2_df,
+                   on= 'NOC',
+                   how='outer')
+
+# drop countries that have never won a medal
+output4.dropna(subset = ['Medal'], inplace=True)
+
+output4.tail(10)
+
+output4.to_csv('results/clean.csv')
